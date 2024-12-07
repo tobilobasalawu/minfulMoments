b45550aea6d608a066735bfcead0fe152fbfeb76
John Name <oluwatobilobasalawu1234@gmail.com>
2024-12-07 20:08:12 +0000

    setting up base components

---
+++ src/app/components/buttons.js
@@ -0,0 +1,9 @@
+const Buttons = () => {
+  return (
+    <div className="">
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default Buttons;
---
+++ src/app/components/header.js
@@ -0,0 +1,12 @@
+import Buttons from "./buttons";
+
+const Header = () => {
+  return (
+    <div className="">
+      <h1>MindfulMoments</h1>
+      <Buttons />
+    </div>
+  );
+}
+
+export default Header;
